9f2c230fc927e4237e50f1663d5638e1ab1ab4f4
vanessabertoletti@gmail.com <vbertoletti@Vanessas-MacBook-Pro.local>
2018-07-14 17:45:40 -0700

    Final Project Upload.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Aloha Project
 
-This is the details of the Aloha Project.
+This is my first web page and first project, using fresh knowledge acquired at RED academy during the Web Development program. 
+
+## Technologies
+
+- HTML
+- CSS
+- Javascript/jQuery
+
+## Personal Learnings 
+
+This project has certainly provided me a much more solid understanding of specially HTML and CSS, and how many nice features I can include on my page just using markup language, stylesheet and simple Javascript/jQuery. Definately looking forward to improve my current knowledge and keep developing and amplifying my skills in order to build awesome, accessible, responsive and interactive websites. 
--- js/aloha.js
+++ js/aloha.js
@@ -1,1 +1,68 @@
-console.log("VANESSA")
+$(document).ready(function() {
+  
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true
+  });
+
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    if ($("#text-input").val() == "") {
+      alert("Please enter a valid e-mail address.");
+    } else {
+      alert("Thanks for subscribing!");
+    }
+  });
+
+
+  // found below on codepen https://codepen.io/Paul34/pen/oxQvEv 
+  var itemCount = 0;
+
+  $('.add-to-cart-button').click(function (){
+    itemCount ++;
+    $('#itemCount').html(itemCount).css('display', 'block');
+  }); 
+
+
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+}); 
